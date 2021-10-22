77ee754853adc832154453c9b42c0f9017a42721
Juliana <julianamilson@hotmail.com>
2021-10-22 19:37:51 -0300

    updating: still not working

--- ft_printf.c
+++ ft_printf.c
@@ -6,35 +6,35 @@
 /*   By: jmilson- <jmilson-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:09:36 by jmilson-          #+#    #+#             */
-/*   Updated: 2021/10/22 18:46:46 by jmilson-         ###   ########.fr       */
+/*   Updated: 2021/10/22 19:32:25 by jmilson-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	get_flag(char *format, va_list argument)
+static int	get_flag(const char *format, va_list argument)
 {
 	int	len;
 
 	len = 0;
-	if (*format == 'c')
-		len = get_c();
-	if (*format == 's')
-		len = get_s();
-	if (*format == 'p')
-		len = get_p();
-	if (*format == 'd')
-		len = get_d();
-	if (*format == 'i')
-		len = get_i();
-	if (*format == 'u')
-		len = get_u();
-	if (*format == 'x')
-		len = get_low_x();
-	if (*format == 'X')
-		len = get_upper_x();
-	if (*format == '%')
-		len = write(1, '%', 1);
+	// if (*format == 'c')
+	// 	len = get_c();
+	// if (*format == 's')
+	// 	len = get_s();
+	// if (*format == 'p')
+	// 	len = get_p();
+	// if (*format == 'd')
+	// 	len = get_d();
+	// if (*format == 'i')
+	// 	len = get_i();
+	// if (*format == 'u')
+	// 	len = get_u();
+	// if (*format == 'x')
+	// 	len = get_low_x();
+	// if (*format == 'X')
+	// 	len = get_upper_x();
+	// if (*format == '%')
+	// 	len = write(1, '%', 1);
 	return (len);
 }
 
@@ -52,8 +52,8 @@ int	ft_printf(const char *format, ...)
 			len += get_flag(format++, arguments);
 			*format++;
 		}
-		len += write (1, &format, 1);
 		*format++;
+		len += write (1, format, 1);
 	}
 	va_end(arguments);
 	return (len);
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jmilson- <jmilson-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:31:11 by jmilson-          #+#    #+#             */
-/*   Updated: 2021/10/22 18:16:00 by jmilson-         ###   ########.fr       */
+/*   Updated: 2021/10/22 19:37:21 by jmilson-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@
  * @return return the number of characters printed (excluding the null byte).
 */
 int	ft_printf(const char *format, ...);
+int	get_c(int c);
+int	get_s(char *s);
 
 #endif
