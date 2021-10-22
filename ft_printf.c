57fd9318263fa3ffbfc53e01513e491b618a9c1c
Juliana <julianamilson@hotmail.com>
2021-10-22 18:31:31 -0300

    Adding files and updating a little bit.

--- ft_printf.c
+++ ft_printf.c
@@ -6,38 +6,38 @@
 /*   By: jmilson- <jmilson-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:09:36 by jmilson-          #+#    #+#             */
-/*   Updated: 2021/10/21 00:32:26 by jmilson-         ###   ########.fr       */
+/*   Updated: 2021/10/22 18:20:47 by jmilson-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "ft_printf.h"
 
-static int	get_flag(char *format)
+static int	get_flag(char *format, va_list ap)
 {
 	if (*format == 'c')
-		get_char();
+		get_c();
 	if (*format == 's')
-		get_string();
+		get_s();
 	if (*format == 'p')
-		get_ptr_hex();
+		get_p();
 	if (*format == 'd')
-		get_dec();
+		get_d();
 	if (*format == 'i')
-		get_int();
+		get_i();
 	if (*format == 'u')
-		get_u_dec();
+		get_u();
 	if (*format == 'x')
-		get_low_hex();
+		get_low_x();
 	if (*format == 'X')
-		get_up_hex();
+		get_upper_x();
 	if (*format == '%')
 		write(1, '%', 1);
 }
 
 int	ft_printf(const char *format, ...)
 {
-	va_list arguments;
-	int	len;
+	va_list	arguments;
+	int		len;
 
 	va_start(arguments, format);
 	len = 0;
@@ -45,11 +45,12 @@ int	ft_printf(const char *format, ...)
 	{
 		if (*format == '%')
 		{
-			get_flag(format++);
+			get_flag(format++, arguments);
 			*format++;
 		}
 		*format++;
 		len++;
 	}
+	va_end(arguments);
 	return (len);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -1,24 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmilson- <jmilson-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:31:11 by jmilson-          #+#    #+#             */
-/*   Updated: 2021/10/21 00:33:25 by jmilson-         ###   ########.fr       */
+/*   Updated: 2021/10/22 18:16:00 by jmilson-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef LIBFTPRINTF_H
-#define LIBFTPRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 # include <unistd.h>
-# include <stdlib.h>
 # include <stdarg.h>
 
 /**
- * @return Upon successful return, these functions return the number of characters printed (excluding the null byte used to end output to strings).
+ * @return return the number of characters printed (excluding the null byte).
 */
 int	ft_printf(const char *format, ...);
 
